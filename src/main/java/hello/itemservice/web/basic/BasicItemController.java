2761e4704efd342c7e6fc28c8a77346eee67366d
S2chanse <seop7379@daum.net>
2023-02-23 20:18:12 +0900

    set PRG

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -34,11 +35,11 @@ public class BasicItemController {
         return "basic/addForm";
     }
     @PostMapping("/add")
-    public String addItem(Item item){
-        itemRepository.save(item);
-
-        //model.addAttribute("item",item); 자동으로 Item 클래스를 model에 바인딩 해준다.
-        return "basic/item";
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
