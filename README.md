eafbb3bf256d5814ef09c8968a849a6752cd4c99
S2chanse <seop7379@daum.net>
2023-02-23 14:50:00 +0900

    make function crud

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Used Spring boot, make product managing web
 # Make Product Shop Used Spring Boot 
 ## make init set gitHub and spring boot 
 ## set domain
+
+## Item Info CRUD function 
--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -24,6 +23,50 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId,Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+    @PostMapping("/add")
+    public String addItem(Item item){
+        itemRepository.save(item);
+
+        //model.addAttribute("item",item); 자동으로 Item 클래스를 model에 바인딩 해준다.
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId,Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+    @PostMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+   /** @PostMapping("/add")
+    public String addItem(@ModelAttribute("item") Item item,Model model){
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+        return "basic/item";
+    }*/
+    /**    @PostMapping("/add")
+    public String addItem(@RequestParam String itemName,@RequestParam Integer price,@RequestParam Integer quantity,Model model){
+        Item item = new Item(itemName,price,quantity);
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+        return "basic/item";
+    }*/
     /**
      * 테스트 아이템
      */
